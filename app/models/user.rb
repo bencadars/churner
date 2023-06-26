3d1c7e089ab12aafbf0711ecdabe6e4a0326112f
benjamin cadars <benjamin.cadars@gmail.com>
2023-06-26 18:09:33 +0200

    fixes again

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :surveys
+  has_many :surveys #as HR
+  has_many :surveys_as_receiver, through: :receivers, source: :surveys
+  has_many :receivers
 end
