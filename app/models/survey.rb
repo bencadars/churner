99eb06641c514b904f1bfcdd0186c950bf7b2abb
benjamin cadars <benjamin.cadars@gmail.com>
2023-06-26 17:58:43 +0200

    fixes again

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -2,6 +2,7 @@ class Survey < ApplicationRecord
   belongs_to :user
   belongs_to :template, optional: true
   has_many :survey_questions
+  has_many :questions, through:survey_questions
   has_many :answers
   has_many :receivers
 end
