567d452872539b4290b4cf064abf8d3e33319878
benjamin cadars <benjamin.cadars@gmail.com>
2023-06-27 14:04:13 +0200

    ajout des seed et validation sur user et survey

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,76 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_26_143957) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_26_150528) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.bigint "receiver_id", null: false
+    t.bigint "survey_id", null: false
+    t.bigint "question_id", null: false
+    t.text "text"
+    t.boolean "boolean"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+    t.index ["receiver_id"], name: "index_answers_on_receiver_id"
+    t.index ["survey_id"], name: "index_answers_on_survey_id"
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.text "text"
+    t.boolean "boolean"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "receivers", force: :cascade do |t|
+    t.bigint "survey_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["survey_id"], name: "index_receivers_on_survey_id"
+    t.index ["user_id"], name: "index_receivers_on_user_id"
+  end
+
+  create_table "survey_questions", force: :cascade do |t|
+    t.bigint "survey_id", null: false
+    t.bigint "question_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_survey_questions_on_question_id"
+    t.index ["survey_id"], name: "index_survey_questions_on_survey_id"
+  end
+
+  create_table "surveys", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.boolean "anonymous"
+    t.bigint "user_id", null: false
+    t.bigint "template_id", null: false
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["template_id"], name: "index_surveys_on_template_id"
+    t.index ["user_id"], name: "index_surveys_on_user_id"
+  end
+
+  create_table "template_questions", force: :cascade do |t|
+    t.bigint "template_id", null: false
+    t.bigint "question_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_template_questions_on_question_id"
+    t.index ["template_id"], name: "index_template_questions_on_template_id"
+  end
+
+  create_table "templates", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -31,4 +98,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_26_143957) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "answers", "questions"
+  add_foreign_key "answers", "receivers"
+  add_foreign_key "answers", "surveys"
+  add_foreign_key "receivers", "surveys"
+  add_foreign_key "receivers", "users"
+  add_foreign_key "survey_questions", "questions"
+  add_foreign_key "survey_questions", "surveys"
+  add_foreign_key "surveys", "templates"
+  add_foreign_key "surveys", "users"
+  add_foreign_key "template_questions", "questions"
+  add_foreign_key "template_questions", "templates"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,174 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+
+puts "cleaning the database (except users)..."
+puts "Cleaning Template_questions..."
+TemplateQuestion.destroy_all
+puts "cleaning templates..."
+Template.destroy_all
+puts "cleaning questions..."
+Question.destroy_all
+puts "Cleaning Surveys..."
+Survey.destroy_all
+
+puts "Creating 6 templates..."
+
+template_resignation = Template.create(
+  name: "Resignation",
+  description: "This survey aims to gather feedback from former employees who resigned, including the reasons for leaving, transition processes, and suggestions for improvement."
+)
+
+template_retirement = Template.create(
+  name: "Retirement",
+  description: "This survey is designed to collect feedback from employees who retired, addressing their overall experience, fondest memories, future plans, and suggestions for passing on responsibilities."
+)
+
+template_termination = Template.create(
+  name: "Termination",
+  description: "This survey focuses on gathering feedback from employees who were terminated, including their perspective on the circumstances, suggestions for improvement, and plans for addressing the termination when discussing it with future employers."
+)
+
+template_layoff = Template.create(
+  name: "Layoff",
+  description: "This survey aims to collect feedback from employees who were laid off, addressing their initial reaction, provided assistance, job search plans, and potential interest in future opportunities with the company."
+)
+
+template_end_contract = Template.create(
+  name: "End of Contract",
+  description: "This survey is designed to gather feedback from employees whose contract has ended, including their overall experience, feedback on the work environment, training programs, and suggestions for improvement."
+)
+
+template_redundancy = Template.create(
+  name: "Redundancy",
+  description: "This survey focuses on gathering feedback from employees affected by redundancy, addressing their experience during the process, support received, future career plans, and suggestions for the company."
+)
+
+puts "Creating 20 questions..."
+# Resignation questions
+question_resignation1 = Question.create(text: "What factors led to your decision to resign?")
+question_resignation2 = Question.create(text: "Have you secured a new employment opportunity, or is it a career transition?")
+question_resignation3 = Question.create(text: "Can you provide feedback on your experience working with the organization?")
+question_resignation4 = Question.create(text: "Is there anything the company could have done differently to retain you as an employee?")
+
+# Retirement questions
+question_retirement1 = Question.create(text: "How long have you been with the organization, and what are your fondest memories from your career here?")
+question_retirement2 = Question.create(text: "What are your plans for retirement, both personally and professionally?")
+question_retirement3 = Question.create(text: "Are there any projects or responsibilities you would like to pass on to a successor?")
+question_retirement4 = Question.create(text: "Would you be interested in any part-time or consulting opportunities within the organization after retirement?")
+
+# Termination questions
+question_termination1 = Question.create(text: "Can you please provide your perspective on the circumstances that led to your termination?")
+question_termination2 = Question.create(text: "Is there anything you feel was misunderstood or misrepresented in the termination decision?")
+question_termination3 = Question.create(text: "How do you plan to address this termination when discussing it with future employers?")
+question_termination4 = Question.create(text: "Do you have any suggestions on how the company can improve its termination processes or employee support systems?")
+
+# Layoff questions
+question_layoff1 = Question.create(text: "How did you learn about the layoff decision, and what was your initial reaction?")
+question_layoff2 = Question.create(text: "Have you been provided with information regarding severance packages or assistance in finding new employment?")
+question_layoff3 = Question.create(text: "How do you plan to approach your job search and manage the impact of the layoff on your career?")
+question_layoff4 = Question.create(text: "Would you consider rejoining the company if opportunities become available in the future?")
+
+
+puts "Creating template_questions..."
+template_questions = [
+  { template_id: template_resignation.id, question_id: question_resignation1.id },
+  { template_id: template_resignation.id, question_id: question_resignation2.id },
+  { template_id: template_resignation.id, question_id: question_resignation3.id },
+  { template_id: template_resignation.id, question_id: question_resignation4.id },
+  { template_id: template_retirement.id, question_id: question_retirement1.id },
+  { template_id: template_retirement.id, question_id: question_retirement2.id },
+  { template_id: template_retirement.id, question_id: question_retirement3.id },
+  { template_id: template_retirement.id, question_id: question_retirement4.id },
+  { template_id: template_termination.id, question_id: question_termination1.id },
+  { template_id: template_termination.id, question_id: question_termination2.id },
+  { template_id: template_termination.id, question_id: question_termination3.id },
+  { template_id: template_termination.id, question_id: question_termination4.id },
+  { template_id: template_layoff.id, question_id: question_layoff1.id },
+  { template_id: template_layoff.id, question_id: question_layoff2.id },
+  { template_id: template_layoff.id, question_id: question_layoff3.id },
+  { template_id: template_layoff.id, question_id: question_layoff4.id },
+]
+
+template_questions.each do |template_question|
+  TemplateQuestion.create(template_question)
+end
+
+
+puts "creating fake users if user list is less than 20..."
+
+departure_types = ["resignation", "retirement", "termination", "layoff", "end of contract", "redundancy"]
+
+# Méthode pour générer une date aléatoire entre deux bornes
+def random_date(start_date, end_date)
+  rand(start_date..end_date)
+end
+
+# Vérifie si un utilisateur avec l'adresse e-mail donnée existe déjà
+def user_exists?(email)
+  User.exists?(email: email)
+end
+
+# Vérifie si les dates sont valides (date d'intégration avant date de départ, dates dans le passé)
+def valid_dates?(date_of_integration, date_of_departure)
+  date_of_integration < date_of_departure && date_of_integration < Date.today && date_of_departure < Date.today
+end
+
+if User.count < 20
+# Création de 10 utilisateurs
+  (20 - User.count).times do
+  first_name = Faker::Name.first_name
+    last_name = Faker::Name.last_name
+    email = Faker::Internet.unique.email
+
+    # Vérifie si l'utilisateur existe déjà avec l'adresse e-mail générée
+    next if user_exists?(email)
+
+    type_of_departure = departure_types.sample
+    date_of_integration = random_date(Date.new(2000, 1, 1), Date.new(2023, 6, 25))
+    date_of_departure = random_date(date_of_integration, Date.new(2023, 6, 25))
+
+    # Vérifie si les dates sont valides
+    next unless valid_dates?(date_of_integration, date_of_departure)
+
+    User.create(
+      first_name: first_name,
+      last_name: last_name,
+      email: email,
+      password: 123456,
+      type_of_departure: type_of_departure,
+      date_of_integration: date_of_integration,
+      date_of_departure: date_of_departure
+    )
+  end
+end
+
+puts "Creating Surveys..."
+
+survey1 = Survey.create(
+  name: "Survey 1",
+  description: "This is the first survey",
+  anonymous: true,
+  user_id: 5,
+  template_id: template_resignation.id,
+  status: "draft"
+)
+
+survey2 = Survey.create(
+  name: "Survey 2",
+  description: "This is the second survey",
+  anonymous: false,
+  user_id: 6,
+  template_id: template_termination.id,
+  status: "active"
+)
+
+survey3 = Survey.create(
+  name: "Survey 3",
+  description: "This is the third survey",
+  anonymous: true,
+  user_id: 7,
+  template_id: template_layoff.id,
+  status: "completed"
+)
