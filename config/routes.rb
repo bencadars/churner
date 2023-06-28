82d550201ff8375076bb93b91b570c9da02c6e9e
benjamin cadars <benjamin.cadars@gmail.com>
2023-06-28 11:20:44 +0200

    save

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,13 @@ Rails.application.routes.draw do
   root to: "pages#dashboard"
 
 
-  
+
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :surveys, only: [:show, :new, :create, :update, :edit]
+  resources :surveys, only: [:show, :new, :create, :update, :edit] do
+    resources :users, only: [:index]
+  end
 end
