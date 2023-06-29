cd24233cd29805dfc7b224c4ae821cc48b07104f
benjamin cadars <benjamin.cadars@gmail.com>
2023-06-29 13:00:36 +0200

    search and filter working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,10 @@ class UsersController < ApplicationController
     @users = User.all
 
     if params[:filter]
-      if [:type_of_departure]
+      if params[:filter][:type_of_departure].present?
         @users = @users.where(type_of_departure: params[:filter][:type_of_departure])
       end
     end
-
     if params[:query].present?
      @users = @users.search_by_first_name_and_last_name(params[:query])
     end
