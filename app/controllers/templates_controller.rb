c60339ded68fb42fec29f267401e29c9c8e95c7b
fouad bamhammmed <fouad@bamhammed.com>
2023-06-30 14:58:34 +0200

    correct

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -1,5 +1,6 @@
 class TemplatesController < ApplicationController
   def index
     @templates = Template.includes(template_questions: :question).all
+    @survey = Survey.find(params[:survey_id])
   end
 end
